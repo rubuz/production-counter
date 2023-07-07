013ea09c7c308fae36ea00f65ec640b39b1046cb
rubuz <rubuz23@gmail.com>
2023-07-07 10:29:37 +0200

    side menu finishing

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,26 @@ import caravan from "./assets/caravan.png";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/*" element={<Counter line={62100} logo={caravan} />} />
-      <Route path="/62100" element={<Counter line={62100} logo={caravan} />} />
-      <Route path="/62200" element={<Counter line={62200} logo={caravan} />} />
-      <Route
-        path="/63000"
-        element={<Counter line={63000} logo={motorhome} />}
-      />
-      <Route path="/63200" element={<Counter line={63200} logo={sonic} />} />
-      <Route path="/65200" element={<Counter line={65200} logo={van} />} />
-      <Route path="/65300" element={<Counter line={65300} logo={van} />} />
-    </Routes>
+    <div className="app-container">
+      <Routes>
+        <Route path="/*" element={<Counter line={62100} logo={caravan} />} />
+        <Route
+          path="/62100"
+          element={<Counter line={62100} logo={caravan} />}
+        />
+        <Route
+          path="/62200"
+          element={<Counter line={62200} logo={caravan} />}
+        />
+        <Route
+          path="/63000"
+          element={<Counter line={63000} logo={motorhome} />}
+        />
+        <Route path="/63200" element={<Counter line={63200} logo={sonic} />} />
+        <Route path="/65200" element={<Counter line={65200} logo={van} />} />
+        <Route path="/65300" element={<Counter line={65300} logo={van} />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/SideMenu.jsx
@@ -0,0 +1,42 @@
+import "./sidemenu.css";
+
+const SideMenu = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <a href="/62100">
+            <span>62100</span>Prikolice 1
+          </a>
+        </li>
+        <li>
+          <a href="/62200">
+            <span>62200 </span>Astela & Alipna
+          </a>
+        </li>
+        <li>
+          <a href="/63000">
+            <span>63000 </span>Avtodomi
+          </a>
+        </li>
+        <li>
+          <a href="/63200">
+            <span>63200 </span>Sonic & Coral Supreme
+          </a>
+        </li>
+        <li>
+          <a href="/65200">
+            <span>65200 </span>Van Bič
+          </a>
+        </li>
+        <li>
+          <a href="/62200">
+            <span>65300 </span>Active Bič
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default SideMenu;
